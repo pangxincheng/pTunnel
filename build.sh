01396bacb1df54764a99bbc5eda8552285eb968c
pangxincheng <pangxincheng@foxmail.com>
2024-04-10 18:24:22 +0800

    [feat] add github workflow

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -e
+
+rm -rf ./release/packages
+mkdir -p ./release/packages
+
+rm -rf ./release/build
+mkdir -p ./release/build
+
+package_dir=
+
+os="linux darwin windows"
+arch="amd64 arm64"
+
+for i in $os
+do
+  for j in $arch
+  do
+    # if i is darwin and j is 386, skip
+    if [ $i = "darwin" ] && [ $j = "386" ]; then
+      continue
+    fi
+    echo "Building $i $j"
+    # build cmd/client/pTunnelClient.go
+    mkdir -p ./release/build/$i-$j
+    mkdir -p ./release/build/$i-$j/cert
+    mkdir -p ./release/build/$i-$j/conf
+    cp -r ./conf/*.example ./release/build/$i-$j/conf
+    cp LICENSE ./release/build/$i-$j
+    cp README.md ./release/build/$i-$j
+    GOOS=$i GOARCH=$j go build -o ./release/build/$i-$j/pTunnelClient cmd/client/pTunnelClient.go
+    GOOS=$i GOARCH=$j go build -o ./release/build/$i-$j/pTunnelServer cmd/server/pTunnelServer.go
+    GOOS=$i GOARCH=$j go build -o ./release/build/$i-$j/pTunnelGenRSAKey cmd/genRSAKey/pTunnelGenRSAKey.go
+    # check whether the os is windows, if yes, add .exe suffix
+    if [ $i = "windows" ]; then
+      mv ./release/build/$i-$j/pTunnelClient ./release/build/$i-$j/pTunnelClient.exe
+      mv ./release/build/$i-$j/pTunnelServer ./release/build/$i-$j/pTunnelServer.exe
+      mv ./release/build/$i-$j/pTunnelGenRSAKey ./release/build/$i-$j/pTunnelGenRSAKey.exe
+    fi
+    # compress the binary with zip and save it in ./release/packages
+    cd ./release/build/$i-$j
+    zip -r ../../packages/pTunnel-$i-$j.zip ./
+    cd ../../..
+  done
+done
+
+rm -rf ./release/build
+
+cd -
+echo "Build success!"
